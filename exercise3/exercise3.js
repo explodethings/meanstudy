9402e7d9e1ac62c6187d1fe77b67fc2cbce4aa20
explodethings <kirill20xx@gmail.com>
2018-12-20 19:13:17 +0200

    almost finished, need to add dynamic recipe pages

--- exercise3/exercise3.js
+++ exercise3/exercise3.js
@@ -1,43 +1,122 @@
 /* eslint-disable require-jsdoc */
 /* eslint-disable no-var */
-var userName = '';
+var usersData, recipesData
+const columnNumber = 4
 
-function JSONFromDisk(filePath, callback) {
+// db path
+const usersPath = 'datasource\\users.json';
+const recipesPath = 'datasource\\recipes.json';
+
+function updateDB(db, path) {
+
+}
+
+function selectDB(filePath, callback) {
   var XMLHTTPReq = new XMLHttpRequest()
   XMLHTTPReq.overrideMimeType('application/json')
   XMLHTTPReq.open('GET', filePath, true)
   XMLHTTPReq.onreadystatechange = function() {
-    if (XMLHTTPReq.readyState === 4 && XMLHTTPReq.status === 200) {
+    if (XMLHTTPReq.readyState == 4) {
       callback(XMLHTTPReq.responseText)
     }
   }
   XMLHTTPReq.send(null)
 }
 
-var userData = JSONFromDisk('/datasource/users/users.json', function(response) {
-  return JSON.parse(response)
+// read database from disk
+selectDB(usersPath, function(response) {
+  usersData = JSON.parse(response)
 })
-var recipesData = JSONFromDisk('/datasource/recipes/recipes.json', function(response) {
-  return JSON.parse(response)
+selectDB(recipesPath, function(response) {
+  recipesData = JSON.parse(response)
 })
 
-// $('#login-form').submit(function() {
-//   if ($('#login-user'))
-// });
+function populateCookbook() {
+  // create recipes 
+  // calculate amount of rows - if there is remainder, do one more row
+  var currRecipeNum = 0
+  var currRowNum = 1
+  var numOfRows = 
+    (Math.floor(recipesData.length / columnNumber)) + ((recipesData.length % columnNumber) ? 1 : 0)
+  for (let index = 0; index < numOfRows; index++) {
+    // create row
+    var row = $(document.createElement('div'))
+    row.attr('class', 'row')
+    currRowNum = 1
+    while ((currRecipeNum !== recipesData.length) && (currRowNum % 5 !== 0)) {
+      // create column
+      var column = $(document.createElement('div'))
+      column.attr('class', 'col-md-' + Math.floor(12 / columnNumber))
+
+      // create recipe card
+      var recipeCard = $(document.createElement('div'))
+      recipeCard.attr('class', 'recipe')
+
+      // create recipe card elements
+      var img = $(document.createElement('img'))
+      var h4 = $(document.createElement('h4'))
+      var p = $(document.createElement('p'))
+
+      // edit the elements
+      // image
+      img.attr('src', recipesData[currRecipeNum]['media'][0])
+      img.attr('width', 250)
+      img.attr('height', 225)
+
+      // header
+      h4.html(recipesData[currRecipeNum]['recipeName'])
+      h4.attr('style', 'font-size: 1.33em')
+
+      // description
+      p.html('Time to prepare: ' + recipesData[currRecipeNum]['timeToPrepare'] + '<br>' +
+             'Time to cook: ' + recipesData[currRecipeNum]['timeToCook'] + '<br>' +
+             'Servings: ' + recipesData[currRecipeNum]['servings'] + '<br>' +
+             'Calories: ' + recipesData[currRecipeNum]['calories'] + '<br>')
+      
+      // append the elements
+      row.append(column)
+      column.append(recipeCard)
+      recipeCard.append(img)
+      recipeCard.append(h4)
+      recipeCard.append(p)
+      currRowNum++
+      currRecipeNum++
+    }
+    // ensure dom finished loading with $
+    $(function() {
+      row.appendTo('.cookbook')
+    })
+  }
+}
 
 // eslint-disable-next-line require-jsdoc
 function userAccessControl() {
-  if (userName) {
-    $('.guest').show();
-    $('#user-greet').text('Hello ' + userName + '!');
-    $('#show-cookbook').load('html/cookbook.html');
+  if (window.sessionStorage.userName) {
+    $('.user').hide();
+    $('#user-greet').text('Hello ' + window.sessionStorage.userName + '!');
+    $('#show-cookbook').load('html/cookbook.html', populateCookbook());
   } else {
     $('.guest').hide();
     $('#user-greet').text('Hello Guest!');
     $('#greet-guest').load('html/guest.html');
-    $('#login-screen').load('html/login.html');
+    $('#login-screen').load('html/login.html', function() {
+      $('#login-form').on('submit', function() {
+        for (user of usersData) {
+          if ($('#login-user').val() === user['username']) {
+            if ($('#login-pass').val() === user['password']) {
+              window.sessionStorage.userName = user['displayName']
+              // window.location.replace('#')
+              return true
+            }
+          }
+          alert('Incorrect credentials!')
+          return false
+        }
+     })     
+    })
   }
 }
+
 $(document).ready(function() {
   $('#nav-temp').load('html/nav.html', userAccessControl);
 });
